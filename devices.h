4c0fcb725f752aac09f480a6974dbeb39c73d07e
Zachary Mar <zach.mar.49@gmail.com>
2024-03-25 00:58:16 -0400

    Implemented processing of mouse and keyboard data to update player and cursor objects

--- devices.h
+++ devices.h
@@ -10,8 +10,8 @@ void init_keyboard();
 // Function used to get data from PS2 devices
 void get_PS2_data(volatile int*, unsigned char*);
 // Function used to process mouse data
-void process_mouse_data(unsigned char*, struct Player*, struct Cursor*);
+void process_mouse_data(unsigned char*, Player*, Cursor*);
 // Function used to process keyboard data
-void process_keyboard_data();
+void process_keyboard_data(unsigned char*, Player*);
 
 #endif
--- structs.h
+++ structs.h
@@ -1,36 +1,53 @@
 #ifndef STRUCTS_H
 #define STRUCTS_H
 
+// Enumeration of the player states
+typedef enum PlayerStates {
+  IDLE,
+  MOVING,
+  SHOOTING,
+  HURT,
+  EVASION,
+  DEAD
+} PlayerStates;
+
 // Struct to store player information
-struct Player{
-    // Position of player
-    unsigned int x_pos;
-    unsigned int y_pos;
-    // Score of the player
-    unsigned int score;
-    // Health of player
-    unsigned char health;
-    // Movement speed of player
-    unsigned char vel;
-    // Stores state the player is in
-    // State is used to determine animation to play
-    // TODO - encode states
-    // e.g. 0x01 - idle, 0x02 - move, ...
-    unsigned char player_state;
-    // Cooldown for shooting
-    unsigned char shoot_cooldown;
-    // Cooldown for ability'
-    unsigned char ability_cooldown;
-    // Used to determine the current frame of the animation
-    unsigned char current_frame;
-    // Used to determine the number of frames in the current animation being played
-    unsigned char frames_in_animation;
-};
+typedef struct Player {
+  // Position of player
+  unsigned int x_pos;
+  unsigned int y_pos;
+  // Score of the player
+  unsigned int score;
+  // Health of player
+  unsigned char health;
+  // Movement speed of player
+  unsigned char vel;
+  // Boolean to determine direction of travel
+  unsigned char right;
+  unsigned char left;
+  unsigned char down;
+  unsigned char up;
+  // Stores state the player is in
+  // State is used to determine animation to play
+  PlayerStates state;
+  // Cooldown for shooting
+  unsigned char shoot_cooldown;
+  // Cooldown for ability'
+  unsigned char ability_cooldown;
+  // Used to determine the current frame of the animation
+  unsigned char current_frame;
+  // Used to determine the number of frames in the current animation being
+  // played
+  unsigned char frames_in_animation;
+} Player;
 
 // Struct to store cursor information
-struct Cursor{
-    // Position of cursor
-    unsigned int x_pos;
-    unsigned int y_pos;
-};  
+typedef struct Cursor {
+  // Position of cursor
+  unsigned int x_pos;
+  unsigned int y_pos;
+  // Speed the cursor moves
+  unsigned char vel;
+} Cursor;
+
 #endif
